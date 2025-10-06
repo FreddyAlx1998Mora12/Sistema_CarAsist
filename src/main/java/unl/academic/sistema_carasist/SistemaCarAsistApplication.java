0642720830f6bbf4bfa34a20a13c767d247f2ff5
Freddy A <freddyalexander998@gmail.com>
2025-10-06 11:33:14 -0500

    primer commit, subiendo proyecto

---
+++ src/main/java/unl/academic/sistema_carasist/SistemaCarAsistApplication.java
@@ -0,0 +1,13 @@
+package unl.academic.sistema_carasist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCarAsistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCarAsistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unl/academic/sistema_carasist/SistemaCarAsistApplicationTests.java
@@ -0,0 +1,13 @@
+package unl.academic.sistema_carasist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCarAsistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
